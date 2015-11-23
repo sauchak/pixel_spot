ffc967fa74e2923561f17c7d4e05eb94986344d9
Christopher Sauchak <csauchak@gmail.com>
2015-11-23 12:49:12 -0800

    add crud routes to config routes for user and spot

--- config/routes.js
+++ config/routes.js
@@ -9,7 +9,15 @@ var usersController   = require('../controllers/users');
 router.get('/', welcomeController.index);
 
 // users resource paths:
-router.get('/users',     usersController.index);
-router.get('/users/:id', usersController.show);
+router.get('/users', usersController.index); // to show a list of users
+router.get('/users/:id', usersController.show); // to show one user
+router.post('/userid/spot', usersController.create); // create a new spot
+router.put('/userid/spot/:id', usersController.edit); // to edit a spot
+router.get('/userid/spots', usersController.index); // to show all of a user's spots
+router.destroy('/userid/spot/:id', usersController.destroy); // to delete a spot
+
+// spots resource paths:
+router.get('/spots', spotsController.show); // to show spots search results
+router.get('/spots/:id', spotsController.show); // to show one spot
 
 module.exports = router;
