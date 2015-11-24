53f6a91fd53b8d188769f6a4791a1fff4f71a18d
Christopher Sauchak <csauchak@gmail.com>
2015-11-23 18:59:33 -0800

    add spot controller index search and test to work in spot view

--- config/routes.js
+++ config/routes.js
@@ -4,6 +4,7 @@ var express = require('express'),
 // Require controllers.
 var welcomeController = require('../controllers/welcome');
 var usersController   = require('../controllers/users');
+var spotsController   = require('../controllers/spots')
 
 // root path:
 router.get('/', welcomeController.index);
@@ -11,13 +12,13 @@ router.get('/', welcomeController.index);
 // users resource paths:
 router.get('/users', usersController.index); // to show a list of users
 router.get('/users/:id', usersController.show); // to show one user
-router.post('/userid/spot', usersController.create); // create a new spot
-router.put('/userid/spot/:id', usersController.update); // to edit a spot
-router.get('/userid/spots', usersController.index); // to show all of a user's spots
-router.delete('/userid/spot/:id', usersController.destroy); // to delete a spot
 
 // spots resource paths:
-router.get('/spots', spotsController.show); // to show spots search results
-router.get('/spots/:id', spotsController.show); // to show one spot
+router.get('/spots', spotsController.index); // to show spots search results
+// router.get('/spots/:id', spotsController.show); // to show one spot
+// router.post('/userid/spot', spotsController.create); // create a new spot
+// router.put('/userid/spot/:id', spotsController.update); // to edit a spot
+// router.get('/:userid/spots', spotsController.spotsforuser); // to show all of a user's spots
+// router.delete('/userid/spot/:id', spotsController.destroy); // to delete a spot
 
 module.exports = router;
--- controllers/spots.js
+++ controllers/spots.js
@@ -3,5 +3,23 @@ var User = require("../models/user");
 
 var index = function (req, res, next) {
 
+  User.find({}, function(error, users){
+    var tagSearch = 'cat';
+    var spots = [];
+    users.forEach(function(user) {
+      spots = spots.concat(user.spots);
+    });
+    spots = spots.filter(function(spot) {
+      var found = false;
+      spot.tags.forEach(function(tag) {
+        if (tag.tag_name === tagSearch) found = true;
+      });
+      return found;
+    });
+    res.render('spots/index', {spots: spots});
+  });
+};
 
+module.exports = {
+  index: index
 }
