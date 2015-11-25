58ccbd2e7be2d90f7a23bf6c77e4066133135561
Christopher Sauchak <csauchak@gmail.com>
2015-11-24 19:06:04 -0800

    router.update routes

--- config/routes.js
+++ config/routes.js
@@ -17,7 +17,7 @@ router.get('/users/:id', usersController.show); // to show one user
 router.get('/spots', spotsController.index); // to show spots search results
 router.get('/spots/:id', spotsController.show); // to show one spot
 router.post('/spots/new', spotsController.create); // create a new spot
-// router.put('/:userid/spot/:id', spotsController.update); // to edit a spot
+router.put('/:user_id/spot/:id', spotsController.update); // to edit a spot
 // router.get('/:userid/spots', spotsController.spotsforuser); // to show all of a user's spots
 // router.delete('/:userid/spot/:id', spotsController.destroy); // to delete a spot
 
