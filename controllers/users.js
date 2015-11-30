334429d0d916942d2459e68c524c9822502c6d78
Wayne Takeda <whtakeda@gmail.com>
2015-11-30 09:53:44 -0800

    Removing add/edit buttons if viewing another user

--- controllers/users.js
+++ controllers/users.js
@@ -4,7 +4,8 @@ var User = require("../models/user");
 var show = function(req, res, next){
   User.findById(req.params.id, function(error, otherUser){
     if (error) res.json({message: 'Could not find user because ' + error});
-    res.render('users/show', {user: req.user, otherUser: otherUser});
+    var valid = (req.user._id.toString() == otherUser._id.toString());
+    res.render('users/show', {user: req.user, otherUser: otherUser, valid: valid});
   });
 };
 
